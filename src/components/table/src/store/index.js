601d5a17d14868f6c58bff1ef5801ae6d87bc9b0
Mignon <mamying@live.com>
2019-09-11 15:43:20 +0800

    fix(table): 挂载时不再触发 sort-change 事件

--- src/components/table/src/store/index.js
+++ src/components/table/src/store/index.js
@@ -70,13 +70,13 @@ Watcher.prototype.mutations = {
   },
 
   sort(states, options) {
-    const { prop, order } = options
+    const { prop, order, init } = options
     if (prop) {
       const column = arrayFind(states.columns, column => column.property === prop)
       if (column) {
         column.order = order
         this.updateSort(column, prop, order)
-        this.commit('changeSortCondition')
+        this.commit('changeSortCondition', { init })
       }
     }
   },
@@ -91,7 +91,7 @@ Watcher.prototype.mutations = {
     const ingore = { filter: true }
     this.execQuery(ingore)
 
-    if (!options || !options.silent) {
+    if (!options || !(options.silent || options.init)) {
       this.table.$emit('sort-change', {
         column,
         prop,
