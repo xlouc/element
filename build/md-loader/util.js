3f44d102a94b35482b1302b3ed7d52cd3e62c6ed
Mignon <mamying@live.com>
2019-09-17 14:58:44 +0800

    docs: 添加在线运行方式

--- build/md-loader/util.js
+++ build/md-loader/util.js
@@ -33,29 +33,33 @@ function genInlineComponentText(template, script) {
   // https://github.com/vuejs/vue-loader/blob/423b8341ab368c2117931e909e2da9af74503635/lib/loaders/templateLoader.js#L46
   const finalOptions = {
     source: `<div>${template}</div>`,
-    filename: 'inline-component', // TODO：这里有待调整
+    filename: "inline-component", // TODO：这里有待调整
     compiler
-  }
-  const compiled = compileTemplate(finalOptions)
+  };
+  const compiled = compileTemplate(finalOptions);
   // tips
   if (compiled.tips && compiled.tips.length) {
     compiled.tips.forEach(tip => {
-      console.warn(tip)
-    })
+      console.warn(tip);
+    });
   }
   // errors
   if (compiled.errors && compiled.errors.length) {
-    console.error(`\n  Error compiling template:\n${pad(compiled.source)}\n` + compiled.errors.map(e => `  - ${e}`).join('\n') + '\n')
+    console.error(
+      `\n  Error compiling template:\n${pad(compiled.source)}\n` +
+        compiled.errors.map(e => `  - ${e}`).join("\n") +
+        "\n"
+    );
   }
   let demoComponentContent = `
     ${compiled.code}
-  `
+  `;
   // todo: 这里采用了硬编码有待改进
-  script = script.trim()
+  script = script.trim();
   if (script) {
-    script = script.replace(/export\s+default/, 'const democomponentExport =')
+    script = script.replace(/export\s+default/, "const democomponentExport =");
   } else {
-    script = 'const democomponentExport = {}'
+    script = "const democomponentExport = {}";
   }
   demoComponentContent = `(function() {
     ${demoComponentContent}
@@ -65,8 +69,8 @@ function genInlineComponentText(template, script) {
       staticRenderFns,
       ...democomponentExport
     }
-  })()`
-  return demoComponentContent
+  })()`;
+  return demoComponentContent;
 }
 
 module.exports = {
